2721e3c4728cd06d74bdd2edaeb8a9953a350e86
SHIVANG TIWARI <thankyoubesti@gmail.com>
2025-04-21 23:47:40 +0530

    Update utils.js

--- backend/config.js
+++ backend/config.js
@@ -3,9 +3,9 @@ dotenv.config();
 
 const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
 const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
-const STRIPE_SECRET_KEY =
-  "sk_test_51RFtAMHFCbjjrjGGDUrXs6oOE8T0SxtcNTnYVz0Uy8ChBlrN4AA75LZIj9BwQ9YliKQ2t0isP11iHCZFwPYJbL6800qLFGtoFS";
-
+const STRIPE_SECRET_KEY =process.env.STRIPE_SECRET_KEY;
+  
+ 
 export default {
   JWT_USER_PASSWORD,
   JWT_ADMIN_PASSWORD,
--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,3 +1,3 @@
 
 
-export const BACKEND_URL  = "http://localhost:4001/api/v1";
+export const BACKEND_URL  = "https://hirewire-5tgk.onrender.com/";
